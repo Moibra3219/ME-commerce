16d732b259d5a485266281914942da0f0b8870b6
Mohamed Ibrahem <mohamed.ibrahem3219@gmail.com>
2024-12-10 04:28:14 +0200

    MA

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,61 +1,46 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "boxicons/css/boxicons.min.css";
 import CollectionCard from "../components/CollectionCard";
 import HeaderDashed from "../components/HeaderDashed";
-// import FetchErrorMsg from "../components/FetchErrorMsg";
 import FetchWaitingMsg from "../components/FetchWaitingMsg";
-
-
-
-
 
 import { motion } from "framer-motion";
 import { ShopContext } from "../context/ShopContext";
 
 const ProductDetails = () => {
-	const {productId} = useParams();
-	const {productsData} = useContext(ShopContext);
+	const { productId } = useParams();
+	const { productsData } = useContext(ShopContext);
 
-	
 	const [productData, setProductData] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [activeSize, setActiveSize] = useState(null);
 	const [activeImage, setActiveImage] = useState(0);
-	
+
 	useEffect(() => {
-		console.log(productsData[0].image[0]);
-		
-		let data = productsData.find(obj => obj._id == productId);
-		data = {...data, rating: {stars: 4.5, count: 122}};
-		setProductData(data);             // the selected product
+		let data = productsData.find((obj) => obj._id === productId);
+		data = { ...data, rating: { stars: 4.5, count: 122 } };
+		setProductData(data); // the selected product
 		setLoading(false);
 	}, [productId, productsData]);
-
 
 	const findRelatedProducts = () => {
 		if (!productData || !productsData.length) {
 			return <h1 className="text-center my-5">Loading...</h1>;
 		}
-		
+
 		const relatedProducts = productsData.filter(
 			(product) =>
-				product.category === productData.category && product._id != productData._id
+				product.category === productData.category && product._id !== productData._id
 		);
-		
+
 		return (
 			<main className="d-flex row-gap-3 flex-wrap mt-4">
-			{relatedProducts.slice(0,5).map((pro,i) => {
-								return <CollectionCard key={i} data={pro}/>
-							})}
+				{relatedProducts.slice(0, 5).map((pro, i) => (
+					<CollectionCard key={i} data={pro} />
+				))}
 			</main>
-		)
-	
-
-
-
-
-
+		);
+	};
 
 	return (
 		<motion.section
@@ -66,8 +51,7 @@ const ProductDetails = () => {
 		>
 			{loading ? (
 				<FetchWaitingMsg />
-			)  
-			: (
+			) : (
 				<div className="container mt-5">
 					<main className="product-wrapper d-flex gap-3 gap-lg-5 flex-wrap align-items-start">
 						{/* Product Images */}
@@ -101,19 +85,14 @@ const ProductDetails = () => {
 								<div className="stars mb-3">
 									{Array(5)
 										.fill()
-										.map(
-											(
-												_,
-												i // i >> 0 : 4
-											) => (
-												<i
-													key={i}
-													className={`bx bxs-star ${
-														productData.rating.stars >= i + 1 ? "active" : ""
-													}`}
-												></i>
-											)
-										)}
+										.map((_, i) => (
+											<i
+												key={i}
+												className={`bx bxs-star ${
+													productData.rating.stars >= i + 1 ? "active" : ""
+												}`}
+											></i>
+										))}
 									<span className="ms-2">({productData.rating.count})</span>
 								</div>
 								<span className="fw-bold fs-2 d-block mb-3">
@@ -126,20 +105,18 @@ const ProductDetails = () => {
 							<div className="select-size mt-4">
 								<span className="c-gray fw-bold">Select Size</span>
 								<div className="size-btns d-flex gap-2 mt-2">
-									{productData.sizes.map((size, i) => {
-										return (
-											<span
-												className={`d-block px-3 py-2 bg-m-gray border-gray trans-3 ${
-													activeSize == size && "active"
-												}`}
-												role="button"
-												key={i}
-												onClick={() => setActiveSize(size)}
-											>
-												{size}
-											</span>
-										);
-									})}
+									{productData.sizes.map((size, i) => (
+										<span
+											className={`d-block px-3 py-2 bg-m-gray border-gray trans-3 ${
+												activeSize === size && "active"
+											}`}
+											role="button"
+											key={i}
+											onClick={() => setActiveSize(size)}
+										>
+											{size}
+										</span>
+									))}
 								</div>
 							</div>
 							{/* Add to Cart Button */}
@@ -170,21 +147,15 @@ const ProductDetails = () => {
 						</ul>
 						<div className="description-text border-gray p-3 c-gray">
 							An e-commerce website is an online platform that facilitates the
-							buying and selling of products or services over the internet. It
-							serves as a virtual marketplace where businesses and individuals
-							can showcase their products, interact with customers, and conduct
-							transactions without the need for a physical presence. <br />
+							buying and selling of products or services over the internet.
+							<br />
 							<br />
 							E-commerce websites have gained immense popularity due to their
 							convenience, accessibility, and the global reach they offer.
-							E-commerce websites typically display products or services along
-							with detailed descriptions, images, prices, and any available
-							variations (e.g., sizes, colors). Each product usually has its own
-							dedicated page with relevant information.
 						</div>
 					</article>
 					{/* Related Products */}
-					<div className="related-products  mt-5">
+					<div className="related-products mt-5">
 						<HeaderDashed
 							head1="RELATED"
 							head2="PRODUCTS"
@@ -195,7 +166,7 @@ const ProductDetails = () => {
 				</div>
 			)}
 		</motion.section>
-	)
+	);
 };
 
 export default ProductDetails;
